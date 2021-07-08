3fdce529f8ef5a3ee657408fa32e7e978b98dc70
Daniel Souza <daniielsouzapvh@gmail.com>
2021-07-07 21:19:36 -0300

    feat: formulario reativo

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  usuario = ''
+  senha = ''
+  constructor(private authService: AutenticacaoService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login(): void {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
+      this.router.navigate(['animais'])
+    }, error => {
+      alert('Usuário ou senha inválidos')
+      console.log(error)
+    }
+    )
+  }
 }
---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,20 @@
+import { Observable } from 'rxjs';
+import { NovoUsuario } from './novo-usuario';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  cadastraNovoUsuario(novoUsuario: NovoUsuario): Observable<NovoUsuario> {
+    return this.httpClient.post<NovoUsuario>('http://localhost:3000/user/signup', novoUsuario)
+  }
+
+  verificaUsuarioExistente(nomeUsuario: string) {
+    return this.httpClient.get(`http://localhost:3000/user/exists/${nomeUsuario}`)
+  }
+}
